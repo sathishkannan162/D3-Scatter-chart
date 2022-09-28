b6a168f13d10314fdbcef49fec1f31e2b0c9f7bd
sathishkannan162 <sathishkannan162@gmail.com>
2022-09-28 22:35:38 +0530

    Removed unwanted comments.

--- dist/script.js
+++ dist/script.js
@@ -14,56 +14,21 @@ append("div").
 attr("id", "tooltip").
 style("opacity", "0");
 
-// console.log(inSeconds("02:30"));
 
 fetch(
 "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json").
 
 then(response => response.json()).
 then(data => {
-  // console.log(data);
-  // console.log(JSON.stringify(data));
-  // data.map(item=>{console.log(item)});
-  // data.map((item) => {
-  //   console.log(item.Year, item.Time);
-  // });
-  // console.log("before svg");
-
-  // let newDataset = data.map(
-  //   (item) => new Date(null, null, null, null, null, item.Seconds)
-  // );
-  // console.log(newDataset);
-  // console.log(
-  //   newDataset[0].toISOString().replace("Z", " ").split(".")[0],
-  //   "hello"
-  // );
-  // let newDate =       newDataset[0].toISOString().replace("Z", " ").split(".")[0];
-  // console.log(newDate);
-  //  let extraDate = new Date(newDate);
-  //  console.log(extraDate);
-
-  // console.log(newDateset[0].toISOString(),"hello");
-
+ 
   let dataset = data.map(item => [
   item.Year,
   item.Time,
-  1000 * (item.Seconds + 30 * 60), // added 3 minutes to account for time zone.
+  1000 * (item.Seconds + 30 * 60), // added 30 minutes to account for time zone difference.
   item.Name,
   item.Nationality,
   item.Doping, new Date(1000 * (item.Seconds + 30 * 60))]);
-  //secondsis in milliseconds
-  console.log(dataset);
-
-  //   console.log(d3.min(dataset, d=>d[2]));
-  //   console.log(d3.max(dataset, d=>d[2]));
-  //   console.log(yScale(2390*1000));
-  // console.log(yAxis(2390));
-
-  // console.log(yAxis);
-  // svg.append("text").text("hello");
-
-  //add y-axis
-  // svg.append('g').attr('id','y-axis').call(yAxis);
+ 
 
   const svg = d3.
   select("#chart").
@@ -72,7 +37,7 @@ then(data => {
   attr("height", h + topMargin + bottomMargin);
 
   svg.append('text').text('Time in minutes').attr('transform','rotate(-90)').attr('x',-330).attr('y',20);
-  svg.append('text').text('Year').attr('x',w/2).attr('y',h+30)
+  svg.append('text').text('Year').attr('x',w/2).attr('y',h+30);
 
   svg.
   append("text").
@@ -124,26 +89,16 @@ then(data => {
   attr("x", 815).
   attr("y", 427);
 
-  // .append('text').attr('id','sub-line').text("35 Fastest times up Alpe d'Huez");
-  // console.log("after svg");
-  // const yScale = d3.scaleTime();
   const yScale = d3.scaleUtc();
-  // let minY = new Date(Date.UTC(0,0,0,0,0,0,d3.min(dataset, (d) => d[2])));
   let minY = new Date(d3.min(dataset, d => d[2]));
-  // let maxY = new Date(Date.UTC(0,0,0,0,0,0,d3.max(dataset, (d) => d[2])));
   let maxY = new Date(d3.max(dataset, d => d[2]));
-  console.log(minY.toString());
-  console.log(maxY);
-  // console.log(minY.toString())
-  // console.log(Date.UTC(0,0,0,0,0,0,2390000),'utc')
+ 
   yScale.domain([minY, maxY]);
-  // yScale.domain([minY, maxY]);
 
   yScale.range([topMargin, h]);
 
   const yAxis = d3.axisLeft(yScale);
   yAxis.tickFormat(d3.timeFormat("%M:%S"));
-  // yAxis.tickFormat(d3.format(''));
   svg.
   append("g").
   attr("id", "y-axis").
@@ -164,7 +119,6 @@ then(data => {
   attr("id", "x-axis").
   attr("transform", "translate(0," + h + ")").
   call(xAxis);
-  // console.log(xScale(1994),"hello");
 
   let bars = svg.
   selectAll("circle").
@@ -181,14 +135,10 @@ then(data => {
   attr("stroke", "black").
   attr("stroke-width", 2).
   on("mouseover", function (event, d) {
-    // console.log("mouseover");
-    // console.log(event.clientX,event.clientY)
 
     tooltip.
     style("opacity", "0.9").
     style('left', event.clientX + 10 + 'px').style('top', event.clientY - 20 + 'px')
-    // .style("left", xScale(d[0]) + 30 + "px")
-    // .style("top", yScale(d[2]) - 5 + "px")
     .html(
     d[3] +
     ": " +
@@ -206,5 +156,4 @@ then(data => {
   on("mouseout", function (event, d) {
     tooltip.style("opacity", "0");
   });
-  // svg.append('rect').attr("height",40).attr("width",40).attr("fill","navy");
 });
