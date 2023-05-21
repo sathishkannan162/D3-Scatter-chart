03e8fd81a952ae21147aef8b95c30dab759d59a6
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-21 17:10:41 +0530

    fix: fetch raw.githubusercontent.com not working

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,358 @@
+let data = [
+{
+  "Time": "36:50",
+  "Place": 1,
+  "Seconds": 2210,
+  "Name": "Marco Pantani",
+  "Year": 1995,
+  "Nationality": "ITA",
+  "Doping": "Alleged drug use during 1995 due to high hematocrit levels",
+  "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use" },
+
+{
+  "Time": "36:55",
+  "Place": 2,
+  "Seconds": 2215,
+  "Name": "Marco Pantani",
+  "Year": 1997,
+  "Nationality": "ITA",
+  "Doping": "Alleged drug use during 1997 due to high hermatocrit levels",
+  "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use" },
+
+{
+  "Time": "37:15",
+  "Place": 3,
+  "Seconds": 2235,
+  "Name": "Marco Pantani",
+  "Year": 1994,
+  "Nationality": "ITA",
+  "Doping": "Alleged drug use during 1994 due to high hermatocrit levels",
+  "URL": "https://en.wikipedia.org/wiki/Marco_Pantani#Alleged_drug_use" },
+
+{
+  "Time": "37:36",
+  "Place": 4,
+  "Seconds": 2256,
+  "Name": "Lance Armstrong",
+  "Year": 2004,
+  "Nationality": "USA",
+  "Doping": "2004 Tour de France title stripped by UCI in 2012",
+  "URL": "https://en.wikipedia.org/wiki/History_of_Lance_Armstrong_doping_allegations" },
+
+{
+  "Time": "37:42",
+  "Place": 5,
+  "Seconds": 2262,
+  "Name": "Jan Ullrich",
+  "Year": 1997,
+  "Nationality": "GER",
+  "Doping": "Confessed later in his career to doping",
+  "URL": "https://en.wikipedia.org/wiki/Jan_Ullrich#Operaci.C3.B3n_Puerto_doping_case" },
+
+{
+  "Time": "38:05",
+  "Place": 6,
+  "Seconds": 2285,
+  "Name": "Lance Armstrong",
+  "Year": 2001,
+  "Nationality": "USA",
+  "Doping": "2001 Tour de France title stripped by UCI in 2012",
+  "URL": "https://en.wikipedia.org/wiki/History_of_Lance_Armstrong_doping_allegations" },
+
+{
+  "Time": "38:14",
+  "Place": 7,
+  "Seconds": 2294,
+  "Name": "Miguel Indurain",
+  "Year": 1995,
+  "Nationality": "ESP",
+  "Doping": "1994 Failed test for salbutemol, not a banned drug at that time",
+  "URL": "http://www.independent.co.uk/sport/drugs-in-sport-indurain-allowed-to-use-banned-drug-1379584.html" },
+
+{
+  "Time": "38:14",
+  "Place": 8,
+  "Seconds": 2294,
+  "Name": "Alex Zülle",
+  "Year": 1995,
+  "Nationality": "SUI",
+  "Doping": "Confessed in 1998 to taking EPO",
+  "URL": "https://en.wikipedia.org/wiki/Alex_Z%C3%BClle#Festina_affair" },
+
+{
+  "Time": "38:16",
+  "Place": 9,
+  "Seconds": 2296,
+  "Name": "Bjarne Riis",
+  "Year": 1995,
+  "Nationality": "DEN",
+  "Doping": "Alleged drug use during 1995 due to high hermatrocite levels",
+  "URL": "https://en.wikipedia.org/wiki/Bjarne_Riis#Doping_allegations" },
+
+{
+  "Time": "38:22",
+  "Place": 10,
+  "Seconds": 2302,
+  "Name": "Richard Virenque",
+  "Year": 1997,
+  "Nationality": "FRA",
+  "Doping": "In 2000 confessed to doping during his career",
+  "URL": "https://en.wikipedia.org/wiki/Richard_Virenque#Festina_affair" },
+
+{
+  "Time": "38:36",
+  "Place": 11,
+  "Seconds": 2316,
+  "Name": "Floyd Landis",
+  "Year": 2006,
+  "Nationality": "USA",
+  "Doping": "Stripped of 2006 Tour de France title",
+  "URL": "https://en.wikipedia.org/wiki/Floyd_Landis_doping_case" },
+
+{
+  "Time": "38:36",
+  "Place": 12,
+  "Seconds": 2316,
+  "Name": "Andreas Klöden",
+  "Year": 2006,
+  "Nationality": "GER",
+  "Doping": "Alleged use of illegal blood transfusions in 2006",
+  "URL": "https://en.wikipedia.org/wiki/Andreas_Kl%C3%B6den#2009_Doping_allegations" },
+
+{
+  "Time": "38:40",
+  "Place": 13,
+  "Seconds": 2320,
+  "Name": "Jan Ullrich",
+  "Year": 2004,
+  "Nationality": "GER",
+  "Doping": "Confessed later in his career to doping",
+  "URL": "https://en.wikipedia.org/wiki/Jan_Ullrich#Operaci.C3.B3n_Puerto_doping_case" },
+
+{
+  "Time": "38:44",
+  "Place": 14,
+  "Seconds": 2324,
+  "Name": "Laurent Madouas",
+  "Year": 1995,
+  "Nationality": "FRA",
+  "Doping": "Tested positive for Salbutemol in 1994, suspended for 1 month",
+  "URL": "http://www.dopeology.org/incidents/Madouas-positive/" },
+
+{
+  "Time": "38:55",
+  "Place": 15,
+  "Seconds": 2335,
+  "Name": "Richard Virenque",
+  "Year": 1994,
+  "Nationality": "FRA",
+  "Doping": "In 2000 confessed to doping during his career",
+  "URL": "https://en.wikipedia.org/wiki/Richard_Virenque#Festina_affair" },
+
+{
+  "Time": "39:01",
+  "Place": 16,
+  "Seconds": 2341,
+  "Name": "Carlos Sastre",
+  "Year": 2006,
+  "Nationality": "ESP",
+  "Doping": "",
+  "URL": "" },
+
+{
+  "Time": "39:09",
+  "Place": 17,
+  "Seconds": 2349,
+  "Name": "Iban Mayo",
+  "Year": 2003,
+  "Nationality": "ESP",
+  "Doping": "Failed doping test in 2007 Tour de France",
+  "URL": "https://en.wikipedia.org/wiki/Iban_Mayo" },
+
+{
+  "Time": "39:12",
+  "Place": 18,
+  "Seconds": 2352,
+  "Name": "Andreas Klöden",
+  "Year": 2004,
+  "Nationality": "GER",
+  "Doping": "Alleged doping during 2006 Tour de France",
+  "URL": "https://en.wikipedia.org/wiki/Operaci%C3%B3n_Puerto_doping_case" },
+
+{
+  "Time": "39:14",
+  "Place": 19,
+  "Seconds": 2354,
+  "Name": "Jose Azevedo",
+  "Year": 2004,
+  "Nationality": "POR",
+  "Doping": "Implicated in the Operación Puerto doping case",
+  "URL": "https://en.wikipedia.org/wiki/Operaci%C3%B3n_Puerto_doping_case" },
+
+{
+  "Time": "39:15",
+  "Place": 20,
+  "Seconds": 2355,
+  "Name": "Levi Leipheimer",
+  "Year": 2006,
+  "Nationality": "USA",
+  "Doping": "Testified in 2012 to doping during his time with US Postal Service ",
+  "URL": "http://www.wsj.com/articles/SB10000872396390444799904578048352672452328" },
+
+{
+  "Time": "39:22",
+  "Place": 21,
+  "Seconds": 2362,
+  "Name": "Francesco Casagrande",
+  "Year": 1997,
+  "Nationality": "ITA",
+  "Doping": "Positive testosterone test in 1998",
+  "URL": "http://autobus.cyclingnews.com/results/1998/sep98/sep2.shtml" },
+
+{
+  "Time": "39:23",
+  "Place": 22,
+  "Seconds": 2363,
+  "Name": "Nairo Quintana",
+  "Year": 2015,
+  "Nationality": "COL",
+  "Doping": "",
+  "URL": "" },
+
+{
+  "Time": "39:23",
+  "Place": 23,
+  "Seconds": 2363,
+  "Name": "Bjarne Riis",
+  "Year": 1997,
+  "Nationality": "DEN",
+  "Doping": "Alleged drug use during 1995 due to high hermatrocite levels",
+  "URL": "https://en.wikipedia.org/wiki/Bjarne_Riis#Doping_allegations" },
+
+{
+  "Time": "39:30",
+  "Place": 24,
+  "Seconds": 2370,
+  "Name": "Miguel Indurain",
+  "Year": 1994,
+  "Nationality": "ESP",
+  "Doping": "1994 Failed test for salbutemol, not a banned drug at that time",
+  "URL": "http://www.independent.co.uk/sport/drugs-in-sport-indurain-allowed-to-use-banned-drug-1379584.html" },
+
+{
+  "Time": "39:30",
+  "Place": 25,
+  "Seconds": 2370,
+  "Name": "Luc Leblanc",
+  "Year": 1994,
+  "Nationality": "FRA",
+  "Doping": "Admitted to using epo and amphetimines throughout 1994 ",
+  "URL": "http://www.dopeology.org/people/Luc_Leblanc/" },
+
+{
+  "Time": "39:32",
+  "Place": 26,
+  "Seconds": 2372,
+  "Name": "Carlos Sastre",
+  "Year": 2008,
+  "Nationality": "ESP",
+  "Doping": "",
+  "URL": "" },
+
+{
+  "Time": "39:37",
+  "Place": 27,
+  "Seconds": 2377,
+  "Name": "Vladimir Poulnikov",
+  "Year": 1994,
+  "Nationality": "UKR",
+  "Doping": "",
+  "URL": "" },
+
+{
+  "Time": "39:40",
+  "Place": 28,
+  "Seconds": 2380,
+  "Name": "Giuseppe Guerini",
+  "Year": 2004,
+  "Nationality": "ITA",
+  "Doping": "",
+  "URL": "" },
+
+{
+  "Time": "39:41",
+  "Place": 29,
+  "Seconds": 2381,
+  "Name": "Santos Gonzalez",
+  "Year": 2004,
+  "Nationality": "ESP",
+  "Doping": "High Hematocrit during 2005 season, removed by team management",
+  "URL": "http://www.cyclingnews.com/news/santos-gonzalez-sacked-by-phonak/" },
+
+{
+  "Time": "39:41",
+  "Place": 30,
+  "Seconds": 2381,
+  "Name": "Vladimir Karpets",
+  "Year": 2004,
+  "Nationality": "RUS",
+  "Doping": "Made payments to Ferrari, but no charges filed",
+  "URL": "http://www.dopeology.org/incidents/Ferrari-investigation/" },
+
+{
+  "Time": "39:45",
+  "Place": 31,
+  "Seconds": 2385,
+  "Name": "Fernando Escartin",
+  "Year": 1995,
+  "Nationality": "ESP",
+  "Doping": "Implicated in Giardini Margherita Raid in 1998 as a 'victim' ",
+  "URL": "http://www.dopeology.org/incidents/Giardini-Margherita-raid-%5bBologna%5d/" },
+
+{
+  "Time": "39:47",
+  "Place": 32,
+  "Seconds": 2387,
+  "Name": "Denis Menchov",
+  "Year": 2006,
+  "Nationality": "RUS",
+  "Doping": "",
+  "URL": "" },
+
+{
+  "Time": "39:47",
+  "Place": 33,
+  "Seconds": 2387,
+  "Name": "Michael Rasmussen",
+  "Year": 2006,
+  "Nationality": "DEN",
+  "Doping": "Admitted to doping with multiple substances 1998-2010",
+  "URL": "http://www.dopeology.org/people/Michael_Rasmussen/" },
+
+{
+  "Time": "39:47",
+  "Place": 34,
+  "Seconds": 2387,
+  "Name": "Pietro Caucchioli",
+  "Year": 2006,
+  "Nationality": "ITA",
+  "Doping": "Associated with Mantova investigation, charges dropped",
+  "URL": "http://www.cyclingnews.com/news/italian-judge-set-to-decide-if-32-named-in-mantova-doping-investigation-should-go-on-trial/" },
+
+{
+  "Time": "39:50",
+  "Place": 35,
+  "Seconds": 2390,
+  "Name": "Nairo Quintana",
+  "Year": 2013,
+  "Nationality": "COL",
+  "Doping": "",
+  "URL": "" }];
+
+
+
 const w = 1000,
-h = 620;
+h = 450;
 const padding = 30;
 const topMargin = 30,
 bottomMargin = 30,
@@ -14,156 +367,152 @@ append("div").
 attr("id", "tooltip").
 style("opacity", "0");
 
-function sepSeconds(time) {
-  let seconds = Number(time.substring(3));
-  return seconds;
-}
-function sepMinutes(time) {
-  let minutes = Number(time.substring(0,2));
-  return minutes;
-}
-console.log(sepMinutes('37:15'));
-console.log(new Date(1999,1,1,0,sepMinutes('37:30'),sepSeconds('37:30')));
 
-fetch(
-"https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json").
+let dataset = data.map(item => [
+item.Year,
+item.Time,
+1000 * (item.Seconds + 30 * 60), // added 3 minutes to account for time zone.
+item.Name,
+item.Nationality,
+item.Doping, new Date(1000 * (item.Seconds + 30 * 60))]);
+//secondsis in milliseconds
+console.log(dataset);
 
-then(response => response.json()).
-then(data => {
- 
-  let dataset = data.map(item => [
-  item.Year,
-  item.Time,
-  1000 * (item.Seconds), 
-  item.Name,
-  item.Nationality,
-  item.Doping, 
-  new Date(1999,1,1,0,sepMinutes(item.Time),sepSeconds(item.Time))
-]);
- 
-  const svg = d3.
-  select("#chart").
-  append("svg").
-  attr("width", w + leftMargin + rightMargin).
-  attr("height", h + topMargin + bottomMargin);
+const svg = d3.
+select("#chart").
+append("svg").
+attr("width", w + leftMargin + rightMargin).
+attr("height", h + topMargin + bottomMargin);
 
-  svg.append('text').text('Time in minutes').attr('transform','rotate(-90)').attr('x',-330).attr('y',20);
-  svg.append('text').text('Year').attr('x',w/2).attr('y',h+30);
+svg.
+append("text").
+attr("id", "title").
+text("Doping in Professional Bicycle Racing").
+attr("x", (w + leftMargin + rightMargin - 440) / 2).
+attr("y", 50).
+style("font-size", 30);
 
-  svg.
-  append("text").
-  attr("id", "title").
-  text("Doping in Professional Bicycle Racing").
-  attr("x", (w + leftMargin + rightMargin - 440) / 2).
-  attr("y", 50).
-  style("font-size", 30);
+svg.
+append("text").
+attr("id", "sub-line").
+text("35 Fastest times up Alpe d'Huez").
+attr("x", (w + leftMargin + rightMargin - 200) / 2).
+attr("y", 75).
+style("font-size", 20);
 
-  svg.
-  append("text").
-  attr("id", "sub-line").
-  text("35 Fastest times up Alpe d'Huez").
-  attr("x", (w + leftMargin + rightMargin - 200) / 2).
-  attr("y", 75).
-  style("font-size", 20);
+//legend
 
+svg.
+append("circle").
+attr("fill", goodColor).
+attr("id", "legend").
+attr("cx", 800).
+attr("cy", 400).
+attr("r", 7).
+attr("stroke", "black").
+attr("stroke-width", 2);
 
-  // legend 
-let legendContainer = svg.append('g').attr("id", "legend")
-.attr('transform','translate(800,200)');
+svg.
+append("circle").
+attr("fill", badColor).
+attr("cx", 800).
+attr("cy", 420).
+attr("r", 7).
+attr("stroke", "black").
+attr("stroke-width", 2);
 
-legendContainer
-.selectAll('.none').data([goodColor,badColor])
-.enter().append('circle')
-.attr('cx',0).attr('cy',(d,i)=>20*i).attr('r',7)
-.attr('fill',d=>d).attr('stroke','black')
-.style('opacity','0.9');
+svg.
+append("text").
+text("No doping allegations").
+attr("x", 815).
+attr("y", 405).
+attr("font-size", 15);
 
-legendContainer.selectAll('.none').data([goodColor,badColor])
-.enter().append('text').text((d,i)=>i==0?'No Doping Allegations':'Riders with Doping Allegations')
-.attr('x',0).attr('y',(d,i)=>20*i)
-.attr('dx',15).attr('dy',5);
+svg.
+append("text").
+text("Riders with doping allegations").
+attr("x", 815).
+attr("y", 427);
 
-legendContainer.append('rect').attr('width',280).attr('height',50)
-.attr('fill','transparent')
-.attr('stroke','black')
-.attr('x',-18).attr('y',-17);
+// .append('text').attr('id','sub-line').text("35 Fastest times up Alpe d'Huez");
+// console.log("after svg");
+// const yScale = d3.scaleTime();
+const yScale = d3.scaleUtc();
+// let minY = new Date(Date.UTC(0,0,0,0,0,0,d3.min(dataset, (d) => d[2])));
+let minY = new Date(d3.min(dataset, d => d[2]));
+// let maxY = new Date(Date.UTC(0,0,0,0,0,0,d3.max(dataset, (d) => d[2])));
+let maxY = new Date(d3.max(dataset, d => d[2]));
+console.log(minY.toString());
+console.log(maxY);
+// console.log(minY.toString())
+// console.log(Date.UTC(0,0,0,0,0,0,2390000),'utc')
+yScale.domain([minY, maxY]);
+// yScale.domain([minY, maxY]);
 
+yScale.range([topMargin, h]);
 
+const yAxis = d3.axisLeft(yScale);
+yAxis.tickFormat(d3.timeFormat("%M:%S"));
+// yAxis.tickFormat(d3.format(''));
+svg.
+append("g").
+attr("id", "y-axis").
+attr("transform", "translate(" + leftMargin + ",0)").
+call(yAxis);
 
-  const yScale = d3.scaleTime();
-  let minY = new Date(d3.min(dataset, d => d[2]));
-  let maxY = new Date(d3.max(dataset, d => d[2]));
- 
-  yScale.domain(d3.extent(dataset,d=>d[6]));
+const xScale = d3.
+scaleLinear().
+domain([
+d3.min(dataset, d => d[0]) - 1,
+d3.max(dataset, d => d[0]) + 1]).
 
-  yScale.range([topMargin, h]);
+range([leftMargin, w]);
+const xAxis = d3.axisBottom(xScale);
+xAxis.tickFormat(d3.format(""));
+svg.
+append("g").
+attr("id", "x-axis").
+attr("transform", "translate(0," + h + ")").
+call(xAxis);
+// console.log(xScale(1994),"hello");
 
-  const yAxis = d3.axisLeft(yScale);
-  yAxis.tickFormat(d3.timeFormat("%M:%S"));
-  svg.
-  append("g").
-  attr("id", "y-axis").
-  attr("transform", "translate(" + leftMargin + ",0)").
-  call(yAxis);
+let bars = svg.
+selectAll("circle").
+data(dataset).
+enter().
+append("circle").
+attr("class", "dot").
+attr("data-xvalue", d => d[0]).
+attr("data-yvalue", d => d[6]).
+attr("fill", d => d[5] === "" ? goodColor : badColor).
+attr("cx", d => xScale(d[0])).
+attr("cy", d => yScale(d[2])).
+attr("r", 7).
+attr("stroke", "black").
+attr("stroke-width", 2).
+on("mouseover", function (event, d) {
+  // console.log("mouseover");
+  // console.log(event.clientX,event.clientY)
 
-  const xScale = d3.
-  scaleLinear().
-  domain([
-  d3.min(dataset, d => d[0]) - 1,
-  d3.max(dataset, d => d[0]) + 1]).
-  range([leftMargin, w]);
+  tooltip.
+  style("opacity", "0.9").
+  style('left', event.clientX + 10 + 'px').style('top', event.clientY - 20 + 'px')
+  // .style("left", xScale(d[0]) + 30 + "px")
+  // .style("top", yScale(d[2]) - 5 + "px")
+  .html(
+  d[3] +
+  ": " +
+  d[4] +
+  "<br/>" +
+  "Year: " +
+  d[0] +
+  ", Time: " +
+  d[1] + (
+  d[5] === "" ? "" : "<br/><br/>") +
+  d[5]).
 
-
-
-  const xAxis = d3.axisBottom(xScale);
-  xAxis.tickFormat(d3.format(""));
-  svg.
-  append("g").
-  attr("id", "x-axis").
-  attr("transform", "translate(0," + h + ")").
-  call(xAxis);
-
-  let dotColor = d3.scaleOrdinal([goodColor,badColor]);
-
-  let circles = svg.
-  selectAll("circle").
-  data(dataset).
-  enter().
-  append("circle").
-  attr("class", "dot").
-  style('opacity','0.9').
-  attr("data-xvalue", d => d[0]).
-  attr("data-yvalue", d => d[6]).
-  attr('fill',d=>dotColor(d[5] === "")).
-  attr("cx", d => xScale(d[0])).
-  attr("cy", d => yScale(d[6])).
-  attr("r", 7).
-  
-  attr("stroke", "black").
-  attr("stroke-width", 1).
-  on("mouseover", function (event, d) {
-
-    tooltip.
-    style("opacity", "0.9").
-    style('left', event.clientX + 10 + 'px').style('top', event.clientY - 20 + 'px')
-    .html(
-    d[3] +
-    ": " +
-    d[4] +
-    "<br/>" +
-    "Year: " +
-    d[0] +
-    ", Time: " +
-    d[1] + (
-    d[5] === "" ? "" : "<br/><br/>") +
-    d[5]).
-
-    attr("data-year", d[0]);
-  }).
-  on("mouseout", function (event, d) {
-    tooltip.style("opacity", "0");
-  });
-
-  
-})
-.catch((error)=>{console.log(error,"hello")});
+  attr("data-year", d[0]);
+}).
+on("mouseout", function (event, d) {
+  tooltip.style("opacity", "0");
+});
